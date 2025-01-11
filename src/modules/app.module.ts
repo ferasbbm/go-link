6340bd7b032704417c78c82f70d027d69eacef19
Feras El-Sharif <49439225+ferasbbm@users.noreply.github.com>
2025-01-11 12:34:33 +0200

    [reClean]: project

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { linkShorteningModule } from './link-shortening.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { AnalyticsModule } from './analytics/analytics.module';
 
 @Module({
-  imports: [linkShorteningModule, AuthModule, UsersModule],
+  imports: [linkShorteningModule, AuthModule, UsersModule, AnalyticsModule],
 })
 export class AppModule {}

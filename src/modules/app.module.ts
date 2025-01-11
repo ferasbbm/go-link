86b65884fecdbd20b3656d0e8982ccb414084c94
Feras El-Sharif <49439225+ferasbbm@users.noreply.github.com>
2025-01-11 12:31:18 +0200

    UserModule: [setup]

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { linkShorteningModule } from './link-shortening.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [linkShorteningModule, AuthModule],
+  imports: [linkShorteningModule, AuthModule, UsersModule],
 })
 export class AppModule {}

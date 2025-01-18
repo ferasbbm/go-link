0c4f0c24b121f8f15c61572b7d7a2ff69914ffc8
Feras El-Sharif <49439225+ferasbbm@users.noreply.github.com>
2025-01-18 10:38:05 +0200

    [NewFeat] add constants to app

---
+++ src/common/types/constants/app.constants.ts
@@ -0,0 +1,1 @@
+export const numberOfNewGeneratedUrlChars = 6;
--- src/modules/links/utils/short-url.generator.ts
+++ src/modules/links/utils/short-url.generator.ts
@@ -1,5 +1,8 @@
 import * as randomstring from 'randomstring';
+import { numberOfNewGeneratedUrlChars } from 'src/common/types/constants/app.constants';
 
-export const generateShortUrl = async (length: number = 6): Promise<string> => {
+export const generateShortUrl = async (
+  length: number = numberOfNewGeneratedUrlChars,
+): Promise<string> => {
   return randomstring.generate(length);
 };

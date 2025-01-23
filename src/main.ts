6c4b1debd8a9c7b7913cb14a0b8e1e69149564f6
Feras El-Sharif <49439225+ferasbbm@users.noreply.github.com>
2025-01-23 09:43:23 +0200

    [update] add route prefix

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   // Enable global validation using ValidationPipe
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
+
+  //Add  route prefix to all routes
+  app.setGlobalPrefix('api');
 
   await app.listen(process.env.PORT ?? 3000);
 }
